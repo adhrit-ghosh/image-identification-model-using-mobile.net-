e8a8d12488c8d53841ce231ff0bb0e5b26b95f0a
adhrit-ghosh <75254357+adhrit-ghosh@users.noreply.github.com>
2021-09-14 19:12:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
-Webcam.set({
-    width:310,
-     height:300,
-      image_format:'png',
-       png_quality:90,
-        constraints:{ 
-            facingMode:"enviorment"}
-});
-camera=document.getElementById("camera");
-Webcam.attach('#camera');
-function take_ss(){
-Webcam.snap(function(data_uri){
-    document.getElementById("snapshot").innerHTML='<img id="captured_image" src="'+data_uri+'">';
-});
-}
-console.log('ml5 version',ml5.version);
-classifier=ml5.imageClassifier('MobileNet',modelLoaded);
-function modelLoaded(){
-    console.log('Model Is Loaded');
-}
-function identify(){
-    img=document.getElementById("captured_image");
-    classifier.classify(img,gotResult);
-}
-function gotResult(error,result){
-if(error){
-    console.error(error);
-    
-}
-else{
-    console.log(result);
-    document.getElementById("object_name").innerHTML=result[0].label;
-}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(525, 310)
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 }
 
 
+function modelLoaded() {
+    console.log("model is loaded ");
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+
+}
